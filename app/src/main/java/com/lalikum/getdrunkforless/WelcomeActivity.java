a0afada369ae1f616cddd475939b62e3b4fc41be
Lajos Balázs Vlasics <vlasicslajos@gmail.com>
2018-04-09 22:04:07 +0200

    added simple welcome screen with animation

--- app/src/main/java/com/lalikum/getdrunkforless/WelcomeActivity.java
+++ app/src/main/java/com/lalikum/getdrunkforless/WelcomeActivity.java
@@ -2,12 +2,30 @@ package com.lalikum.getdrunkforless;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    TextView welcomeTitleTextView;
+    Button getStartedButton;
+    Animation fromTopAnimation;
+    Animation fromBottomAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        welcomeTitleTextView = findViewById(R.id.welcomeTitleTextView);
+        getStartedButton = findViewById(R.id.getStartedButton);
+
+        fromTopAnimation = AnimationUtils.loadAnimation(this, R.anim.from_top);
+        fromBottomAnimation = AnimationUtils.loadAnimation(this, R.anim.from_bottom);
+
+        welcomeTitleTextView.setAnimation(fromTopAnimation);
+        getStartedButton.setAnimation(fromBottomAnimation);
     }
 }
