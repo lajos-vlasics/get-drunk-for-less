201553e2d056c345f0b739ef9db0b90af9286a82
Lajos Balázs Vlasics <vlasicslajos@gmail.com>
2018-04-10 19:04:44 +0200

    added SugarORM to project

---
+++ app/src/main/java/com/lalikum/getdrunkforless/model/Beverage.java
@@ -0,0 +1,13 @@
+package com.lalikum.getdrunkforless.model;
+
+public class Beverage {
+
+    private int id;
+    private String name;
+    private float size;
+    private float alcohol;
+    private float price;
+    private int quantity;
+    private float drunkValue;
+
+}
---
+++ app/src/main/java/com/lalikum/getdrunkforless/model/Options.java
@@ -0,0 +1,10 @@
+package com.lalikum.getdrunkforless.model;
+
+public class Options {
+
+    int id;
+    String userName;
+    String unitType;
+    String currency;
+
+}
