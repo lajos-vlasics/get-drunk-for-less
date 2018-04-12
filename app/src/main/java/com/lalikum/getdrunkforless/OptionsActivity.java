0f32a2863859cdcaab5055e1838ed59c250f7e30
Lajos Balázs Vlasics <vlasicslajos@gmail.com>
2018-04-12 15:17:16 +0200

    option input text validation and save button checking fixed

--- app/src/main/java/com/lalikum/getdrunkforless/OptionsActivity.java
+++ app/src/main/java/com/lalikum/getdrunkforless/OptionsActivity.java
@@ -3,11 +3,16 @@ package com.lalikum.getdrunkforless;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.lalikum.getdrunkforless.controller.OptionsController;
 import com.lalikum.getdrunkforless.model.MeasurementSystem;
@@ -26,6 +31,8 @@ public class OptionsActivity extends AppCompatActivity {
     RadioButton imperialRadioButton;
     RadioGroup unitRadioGroup;
 
+    Button optionsOkButton;
+
     OptionsController optionsController = new OptionsController();
 
     @Override
@@ -38,12 +45,13 @@ public class OptionsActivity extends AppCompatActivity {
         metricRadioButton = findViewById(R.id.metricRadioButton);
         imperialRadioButton = findViewById(R.id.imperialRadioButton);
         unitRadioGroup = findViewById(R.id.unitRadioGroup);
+        optionsOkButton = findViewById(R.id.optionsOkButton);
 
         // set unit fields from ENUM
         metricRadioButton.setText(String.format("Metric (%s)", MeasurementSystem.METRIC.getUnit()));
         imperialRadioButton.setText(String.format("Imperial (%s)", MeasurementSystem.IMPERIAL.getUnit()));
 
-        // If options exists fill from DB
+        // If options exists fill from DB and enable Save button
         if (optionsController.isOptionsExists()) {
             Options options = optionsController.getInstance();
 
@@ -61,12 +69,74 @@ public class OptionsActivity extends AppCompatActivity {
             }
 
             currencyEditText.setText(options.getCurrency());
+            optionsOkButton.setEnabled(true);
         }
+
+        userNameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                setEmptyInputError(userNameEditText);
+                setSaveButtonCheck();
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        currencyEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                setEmptyInputError(currencyEditText);
+                setSaveButtonCheck();
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        userNameEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    setEmptyInputError(userNameEditText);
+                    setSaveButtonCheck();
+                }
+            }
+        });
+
+        currencyEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    setEmptyInputError(currencyEditText);
+                    setSaveButtonCheck();
+                }
+            }
+        });
+
+        currencyEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView view, int actionId, KeyEvent event) {
+                saveOptions(view);
+                return false;
+            }
+        });
     }
 
     public void saveOptions(View view) {
         // TODO check if null
-        if (checkIfInputsAreEmpty(userNameEditText, currencyEditText)) {
+        if (setEmptyInputError(userNameEditText, currencyEditText)) {
             return;
         }
         userName = userNameEditText.getText().toString();
@@ -98,9 +168,8 @@ public class OptionsActivity extends AppCompatActivity {
         }
     }
 
-    private boolean checkIfInputsAreEmpty(EditText... editTextList) {
+    private boolean setEmptyInputError(EditText... editTextList) {
         boolean isEmptyInput = false;
-
         for (EditText editText : editTextList) {
             String inputText = editText.getText().toString();
             if (TextUtils.isEmpty(inputText)) {
@@ -108,7 +177,27 @@ public class OptionsActivity extends AppCompatActivity {
                 isEmptyInput = true;
             }
         }
-
         return isEmptyInput;
     }
+
+    private boolean isEmptyInput(EditText... editTextList) {
+        boolean isEmptyInput = false;
+        for (EditText editText : editTextList) {
+            String inputText = editText.getText().toString();
+            if (TextUtils.isEmpty(inputText)) {
+                isEmptyInput = true;
+            }
+        }
+        return isEmptyInput;
+    }
+
+    private void setSaveButtonCheck() {
+        boolean isEmpty = isEmptyInput(userNameEditText, currencyEditText);
+        if (isEmpty) {
+            optionsOkButton.setEnabled(false);
+        } else {
+            optionsOkButton.setEnabled(true);
+        }
+    }
+
 }
